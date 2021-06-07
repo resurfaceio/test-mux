01ce0600452907a9c7875d3196dc4d02bfb5b7fb
kolin-newby <kolin.newby@gmail.com>
2021-06-07 16:21:11 -0600

    Finished static file server with mux

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module github.com/resurfaceio/test-mux
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// directory of static files to be served
+	dir := "./views/"
+
+	r := mux.NewRouter()
+	// This serves files at http://localhost:4000/static/helloworld.HTML
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":4000",
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
