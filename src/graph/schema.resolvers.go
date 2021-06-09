1ba7ecd63124d44472d0574a86f6c97ff427e3ed
kolin-newby <kolin.newby@gmail.com>
2021-06-09 14:40:38 -0600

    Updated API server file to use mux and added a dummy auto response for graphql query
    Sorry for the big commit

--- src/graph/schema.resolvers.go
+++ src/graph/schema.resolvers.go
@@ -11,12 +11,37 @@ import (
 	"github.com/resurfaceio/test-mux/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+	var Link model.Link
+	var User model.User
+	Link.Address = input.Address
+	Link.Title = input.Title
+	User.Name = "test"
+	Link.User = &User
+	return &Link, nil
+}
+
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
 	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	var links []*model.Link
+	dummyLink := model.Link{
+		Title:   "Resurface test link",
+		Address: "https://resurface.io",
+		User:    &model.User{Name: "pepper"},
+	}
+	links = append(links, &dummyLink)
+	return links, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
--- src/server.go
+++ src/server.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/mux"
 	"github.com/resurfaceio/test-mux/graph"
 	"github.com/resurfaceio/test-mux/graph/generated"
 )
 
 const defaultPort = "8080"
+
+type message struct {
+	msg string
+}
+
+func pong() []byte { // return JSON response as a byte slice
+	m := message{"pong"}
+	j, err := json.Marshal(m)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return j
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,13 +36,17 @@ func main() {
 		port = defaultPort
 	}
 
-	// r := mux.NewRouter()
+	router := mux.NewRouter()
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+	// router.Handle("/ping", pong())
+
+	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
